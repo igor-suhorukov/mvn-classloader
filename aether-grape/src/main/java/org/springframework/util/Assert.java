c27f4aa236e694992407b8927b7e09bf552434f9
igor-suhorukov <igor.suhorukov@gmail.com>
2016-01-22 21:24:20 +0300

    fix ClassNotFound exception for util dependency com.github.igorsuhorukov.springframework.util.Assert

---
+++ aether-grape/src/main/java/org/springframework/util/Assert.java
@@ -0,0 +1,11 @@
+package org.springframework.util;
+
+/**
+ */
+public class Assert {
+    public static void notNull(Object object, String message) {
+        if(object == null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
